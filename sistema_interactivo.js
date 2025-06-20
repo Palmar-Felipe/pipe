e6405a2b12b26cda3f987e6a63dba2da5bc2f6fa
felipe miguel palmar ramirez <pipepalmar@hotmail.com>
2025-06-19 19:57:27 -0500

    feat:add files

---
+++ sistema_interactivo.js
@@ -0,0 +1,16 @@
+
+
+const nombre=document.getElementById("name");
+const numberInput=document.getElementById("number");
+const enviar=document.getElementById("answer")
+boton.addEventListener("click", function (){
+    number = numberInput.value
+    if (number<18) {
+        enviar.textContent = 'Hola , eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!';    
+    }  else if (number >=18){
+        enviar.textContent="Hola [nombre], eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!";
+    
+    }    
+
+})
+`fgjhfghgf${name}`
