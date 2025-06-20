65af7a3352dc079fbea489676c7bdf13de2a9eae
felipe miguel palmar ramirez <pipepalmar@hotmail.com>
2025-06-20 18:24:21 -0500

    feat: complete to proyect in Js

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# pipe
+# README-M3-S1
+
+Este proyecto tiene como finalidad hacer un sistema interactivo para los usuarios que lo utilicen. fue creada con JAVASCRIPT, HTML Y CSS, puede interacturar en las casillas, esta pagina tiene solo una seccion:
+
+1)Validacion de edades
+
+ESTRUCTURA
+.PIPE
+    src
+        java-script
+            sistema_interactivo.js
+        stylesscss
+            styles.css
+ index.html
+ LICENSE
+ README.MD           
+
+
+ como haceder al proyecto para haceder al proyecto seguir los siguientes pasos
+1 git clone 
+2 cd PRUEBA-DE-HTML-CSS 
+3 java-script 
+4 index.HTML Y styles.css 
+
+
+Nombre: Felipe Miguel Palmar Ramirez Clan: Caiman Correo electronico: pipepalmar@hotmail.com Cedula: 1119392593
--- src/Java-script/sistema_interactivo.js
+++ src/Java-script/sistema_interactivo.js
@@ -1,21 +1,42 @@
 
-
+/*en esta seccion declaramos variables*/ 
 const nombre = document.getElementById("name");
 const numberInput = document.getElementById("number");
 const enviar = document.getElementById("answer");
 const boton = document.getElementById("enviar");
 
+/*al hacer click en enviar se cumplen estas condiciones  */
 boton.addEventListener("click", function (e) {
     e.preventDefault(); // Evita que el formulario se recargue
-
-    const number = Number(numberInput.value);
+    
+    const numberValor = numberInput.value;
     const nombreValor = nombre.value;
+    
+    if (numberValor === "") {
+        enviar.textContent = "El campo no puede estar vacío.";
+        return;
+    }
+   
+    // Validar número válido y mayor a cero
+    if (isNaN(number)) {
+        enviar.textContent = "Por favor ingresa un número válido.";
+        return;
+    }
 
+    if (number <= 0) {
+        enviar.textContent = "Por favor ingresa un número mayor a cero.";
+        return;
+    }
+
+    
     if (number < 18) {
         enviar.textContent = `Hola ${nombreValor}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`;    
     } else if (number >= 18) {
         enviar.textContent = `Hola ${nombreValor}, eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!`;
-    }
+    } 
+
+  
+
 });
 
 
