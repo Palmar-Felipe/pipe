68fac4e088ba0645e071ee09f81c993f4181d54e
pipe <pipepalmar@hotmail.com>
2025-06-20 09:34:13 -0500

    feat:cree estilos

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,16 +1,21 @@
 
 
-const nombre=document.getElementById("name");
-const numberInput=document.getElementById("number");
-const enviar=document.getElementById("answer")
-boton.addEventListener("click", function (){
-    number = numberInput.value
-    if (number<18) {
-        enviar.textContent = 'Hola , eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!';    
-    }  else if (number >=18){
-        enviar.textContent="Hola [nombre], eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!";
-    
-    }    
+const nombre = document.getElementById("name");
+const numberInput = document.getElementById("number");
+const enviar = document.getElementById("answer");
+const boton = document.getElementById("enviar");
 
-})
-`fgjhfghgf${name}`
+boton.addEventListener("click", function (e) {
+    e.preventDefault(); // Evita que el formulario se recargue
+
+    const number = Number(numberInput.value);
+    const nombreValor = nombre.value;
+
+    if (number < 18) {
+        enviar.textContent = `Hola ${nombreValor}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`;    
+    } else if (number >= 18) {
+        enviar.textContent = `Hola ${nombreValor}, eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!`;
+    }
+});
+
+
